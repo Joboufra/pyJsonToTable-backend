960277f05da99b8a83275e3d6176f3de38625b29
Joboufra <joboufra@gmail.com>
2023-11-26 14:06:44 +0100

    update CORS

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["https://10.20.10.10:3003","http://10.20.10.10:3002"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],
